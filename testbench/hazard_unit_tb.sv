7f54948aa4ca171ac83b4aa74dfaabc649238c76
Fengjian Pan <pan81@purdue.edu>
2015-10-01 10:50:13 -0400

    merge

--- testbench/hazard_unit_tb.sv
+++ testbench/hazard_unit_tb.sv
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-`include "cpu_types_pkg.vh"
-`include "hazard_unit_if.vh"
-`timescale 1 ns / 1 ns
-parameter PERIOD = 10;
-import cpu_types_pkg::*;
-
-module hazard_unit_tb;
-   hazard_unit_if huif;
-   logic 
-   
-=======
 `include "hazard_unit_if.vh"
 `include "cpu_types_pkg.vh"
 import cpu_types_pkg::*;
