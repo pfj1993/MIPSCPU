ebc05515c92a3bd8da0dda44c955dd1aeeccc924
Fengjian Pan <pan81@purdue.edu>
2015-10-24 00:53:58 -0400

    script fixed

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ if [[ $1 == '-s' ]];then
     asm $2
     sim -t > /dev/null
     make system.sim
+    diff memsim.hex memcpu.hex
 else
     asm $1
     sim -t > /dev/null
     make system.wav
-    diff memsim.hex memcpu.hex
 fi
 exit 0
